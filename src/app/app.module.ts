b6012291f3037fc59a9c244368d55598ce55ea18
Jeremy Hurley <hurleyjer@yahoo.com>
2017-03-10 16:21:49 -0500

    dummy table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LeaderTableComponent } from './leader-table/leader-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LeaderTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/leader-table/leader-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cl-leader-table',
+  templateUrl: './leader-table.component.html',
+  styleUrls: ['./leader-table.component.css']
+})
+export class LeaderTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
